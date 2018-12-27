bd7b6ee68edca285be815613976c3054f289a732
Yining Shi <ys2189@nyu.edu>
2018-12-27 18:25:00 -0500

    setup

---
+++ website/pages/en/index.js
@@ -0,0 +1,254 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    // const Logo = props => (
+    //   <div className="projectLogo">
+    //     <img src={props.img_src} alt="Project Logo" />
+    //   </div>
+    // );
+
+    const ProjectTitle = () => (
+      <h2 className="projectTitle">
+        {siteConfig.title}
+        <small>{siteConfig.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = props => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        {/* <Logo img_src={`${baseUrl}img/docusaurus.svg`} /> */}
+        <div className="inner">
+          <ProjectTitle siteConfig={siteConfig} />
+          <PromoSection>
+            <Button href={docUrl('getstarted.html')}>Try It Out</Button>
+            <Button href={docUrl('example1.html')}>Examples</Button>
+            <Button href='/blog'>Tutorials</Button>
+          </PromoSection>
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Code extends React.Component {
+  render() {
+    return (
+      <div className="codeContainer">
+        <h2 className="center">
+          Connect to a BLE device and read from one characteristic:
+        </h2>
+        <div className="codeBox">
+          <div className="codeExample">
+            <p className="code bold">
+              // Connect to a BLE device by passing the service UUID
+            </p>
+            <p className="code mb20">
+              p5ble.<span className="codehl3">connect</span>(<span className="codehl2">'19b10010-e8f2-537e-4f6c-d104768a1214'</span>, gotCharacteristics);
+            </p>
+            <p className="code bold">
+            // Got Characteristics
+            </p>
+            <p className="code">
+            <span className="codehl">function</span> <span className="codehl3">gotCharacteristics</span>(error, characteristics) &#123;
+            </p>
+              <p className="code">
+              &nbsp;&nbsp;console.<span className="codehl3">log</span>(characteristics);
+              </p>
+              <p className="code">
+              &nbsp;&nbsp;myCharacteristic = characteristics[0];
+              </p>
+              <p className="code bold">
+              &nbsp;&nbsp;// Read the value of the first characteristic
+              </p>
+              <p className="code">
+              &nbsp;&nbsp;p5ble.<span className="codehl3">read</span>(myCharacteristic, gotValue);
+              </p>
+            <p className="code mb20">
+              &#125;
+            </p>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="left"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    // const FeatureCallout = () => (
+    //   <div
+    //     className="productShowcaseSection paddingBottom"
+    //     style={{textAlign: 'center'}}>
+    //     <h2>Feature Callout</h2>
+    //     <MarkdownBlock>These are features of this project</MarkdownBlock>
+    //   </div>
+    // );
+
+    const TryOut = () => (
+      <Block id="try">
+        {[
+          {
+            content: 'Talk about trying this out',
+            image: `${baseUrl}img/docusaurus.svg`,
+            imageAlign: 'left',
+            title: 'Get ',
+          },
+        ]}
+      </Block>
+    );
+
+    const Description = () => (
+      <Block background="light">
+        {[
+          {
+            content: 'With p5.ble.js, you can request and connect to nearby Bluetooth devices, read/write Bluetooth characteristics, receive GATT Notifications, know if a Bluetooth device is connected.',
+            title: 'Features',
+          },
+          {
+            content: 'The library is supported by code examples, tutorials that cover many popular Bluetooth LE devices. p5.ble.js is inspired by p5.serialport, p5.js, and Web Bluetooth API.',
+            title: 'More',
+          }
+        ]}
+      </Block>
+    );
+
+    const LearnHow = () => (
+      <a href="/docs/example1">
+        <Block>
+        {[
+          {
+            content: 'Checkout Out all the examples and tutorials!',
+            image: `${baseUrl}img/Bluetooth-logo.png`,
+            imageAlign: 'right',
+            title: 'Examples and Tutorials',
+          },
+        ]}
+      </Block>
+      </a>
+    );
+
+    // const Features = () => (
+    //   <Block layout="fourColumn">
+    //     {[
+    //       {
+    //         content: 'This is the content of my feature',
+    //         image: `${baseUrl}img/docusaurus.svg`,
+    //         imageAlign: 'top',
+    //         title: 'Feature One',
+    //       },
+    //       {
+    //         content: 'The content of my second feature',
+    //         image: `${baseUrl}img/docusaurus.svg`,
+    //         imageAlign: 'top',
+    //         title: 'Feature Two',
+    //       },
+    //     ]}
+    //   </Block>
+    // );
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter(user => user.pinned)
+        .map(user => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Description />
+          {/* <TryOut /> */}
+          <Code />
+          <LearnHow />
+          {/* <Showcase /> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
---
+++ website/siteConfig.js
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// See https://docusaurus.io/docs/site-config for all the possible
+// site configuration options.
+
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
+    // image: '/img/docusaurus.svg',
+    infoLink: 'https://yining1023.github.io',
+    pinned: true,
+  },
+];
+
+const siteConfig = {
+  title: 'p5.ble.js', // Title for your website.
+  tagline: 'A Javascript library that enables communication between BLE devices and p5 sketches.',
+  url: 'https://yining1023.github.io', // Your website URL
+  baseUrl: '/p5ble-website/',
+  baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
+
+  // Used for publishing and more
+  projectName: 'p5ble-website',
+  organizationName: 'yining1023',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'getstarted', label: 'Get Started'},
+    {doc: 'api', label: 'API'},
+    {doc: 'example1', label: 'Examples'},
+    {href: 'https://github.com/yining1023/p5.ble.js', label: 'Code'},
+    {blog: true, label: 'Tutorials'},
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  // headerIcon: 'img/docusaurus.svg',
+  // footerIcon: 'img/docusaurus.svg',
+  // favicon: 'img/favicon.png',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#5885ff',
+    secondaryColor: '#3452a3',
+  },
+
+  /* Custom fonts for website */
+  /*
+  fonts: {
+    myFont: [
+      "Times New Roman",
+      "Serif"
+    ],
+    myOtherFont: [
+      "-apple-system",
+      "system-ui"
+    ]
+  },
+  */
+
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  // copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
+
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  // No .html extensions for paths.
+  cleanUrl: true,
+
+  // Open Graph and Twitter card images.
+  // ogImage: 'img/docusaurus.png',
+  // twitterImage: 'img/docusaurus.png',
+
+  // Show documentation's last contributor's name.
+  // enableUpdateBy: true,
+
+  // Show documentation's last update time.
+  // enableUpdateTime: true,
+
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  repoUrl: 'https://github.com/yining1023/p5.ble.js',
+};
+
+module.exports = siteConfig;
